5369f86be23cd34060648c9df60304976fa4570e
Draakje10 <draakje11111@gmail.com>
2026-01-01 17:49:02 +0100

    chore: added all necessary classes to just attach all new hardware to the patient

--- addons/circulation/ACE_Medical_Treatment_Actions.hpp
+++ addons/circulation/ACE_Medical_Treatment_Actions.hpp
@@ -3,8 +3,48 @@
 
 class ACE_Medical_Treatment_Actions 
 {
-    class CPR
+    class CPR;
+    class AEDXVehicleConnectVitalsMonitor;
+    class AEDXDisconnectVitalsMonitor;
+    class AEDXVehiclePlacePads;
+    class DefibrillatorRemovePads;
+    class ViewMonitor;
 
-    class kap_LUCASDevice : CPR
+    class kap_PlaceLUCASDevice : CPR {
+        
+    }
+
+    class kap_RemoveLUCASDevice : CPR {
+        
+    }
+
+    class kap_ActivateLUCASDevice : CPR {
+        
+    }
+
+    class kap_DeactivateLUCASDevice : CPR {
+        
+    }
+
+    class kap_AttachVehicleVitalsMonitor : AEDXVehicleConnectVitalsMonitor {
+        
+    }
+
+    class kap_DetachVehicleVitalsMonitor : AEDXDisconnectVitalsMonitor {
+        
+    }
+
+    class kap_AttachVehicleDefibrillator : AEDXVehiclePlacePads {
+        
+    }
+
+    class kap_DetachVehicleDefibrillator : DefibrillatorRemovePads {
+        
+    };
+
+    class kap_viewVehicleMonitor : ViewMonitor {
+        
+    };
+
     
 };
